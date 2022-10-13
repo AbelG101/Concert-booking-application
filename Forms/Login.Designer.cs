6287d1beb9807d34ac1404ff5b395a94dc4839c7
AbelG101 <abelgebeyehu101@gmail.com>
2022-10-13 21:53:53 +0300

    add login form

---
+++ Forms/Login.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace FinalProject.Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txtBox_userName = new System.Windows.Forms.TextBox();
+            this.txtBox_psswd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBox_userName
+            // 
+            this.txtBox_userName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(89)))), ((int)(((byte)(100)))));
+            this.txtBox_userName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_userName.Font = new System.Drawing.Font("Product Sans", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_userName.ForeColor = System.Drawing.Color.White;
+            this.txtBox_userName.Location = new System.Drawing.Point(158, 164);
+            this.txtBox_userName.Name = "txtBox_userName";
+            this.txtBox_userName.Size = new System.Drawing.Size(283, 28);
+            this.txtBox_userName.TabIndex = 0;
+            // 
+            // txtBox_psswd
+            // 
+            this.txtBox_psswd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(89)))), ((int)(((byte)(100)))));
+            this.txtBox_psswd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBox_psswd.Font = new System.Drawing.Font("Product Sans", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_psswd.ForeColor = System.Drawing.Color.White;
+            this.txtBox_psswd.Location = new System.Drawing.Point(158, 207);
+            this.txtBox_psswd.Name = "txtBox_psswd";
+            this.txtBox_psswd.PasswordChar = '●';
+            this.txtBox_psswd.Size = new System.Drawing.Size(283, 28);
+            this.txtBox_psswd.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Product Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(48, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Product Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(56, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(179)))), ((int)(((byte)(13)))));
+            this.btn_login.FlatAppearance.BorderSize = 0;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.Black;
+            this.btn_login.Location = new System.Drawing.Point(158, 252);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(283, 35);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Product Sans", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(259, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "LOGIN";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Product Sans", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(155, 551);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Don\'t have an account yet? ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(179)))), ((int)(((byte)(13)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Product Sans", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(373, 547);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Signup";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(523, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(471, 583);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(1011, 607);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox_psswd);
+            this.Controls.Add(this.txtBox_userName);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_userName;
+        private System.Windows.Forms.TextBox txtBox_psswd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Forms/Login.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FinalProject.Database;
+using FinalProject.Models;
+
+namespace FinalProject.Forms
+{
+    public partial class Login : Form
+    {
+        string userName;
+        string psswd;
+        public Login()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            userName = txtBox_userName.Text;
+            psswd = txtBox_psswd.Text;
+            string query = $@"SELECT * FROM Customer WHERE user_name = @userName AND password = @psswd;";
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@userName", userName));
+            parameters.Add(new SqlParameter("@psswd", psswd));
+            if (userIsValid(query, parameters))
+            {
+                Customer customer = getCustomerDetails(query, parameters);
+                this.Hide();
+                Form homeForm;
+                if ( customer.UserName.ToLower() == "admin" && customer.Psswd.ToLower() == "admin123")
+                {
+                    homeForm = new AdminForm();
+                }
+                else
+                    homeForm = new Concerts(customer);
+                homeForm.Closed += (s, args) => Close();
+                homeForm.Show();
+            } else
+            {
+                MessageBox.Show("User name or password is incorrect!");
+            }
+        }
+
+        private Customer getCustomerDetails(string query, List<SqlParameter> parameters)
+        {
+            DataRow foundUser = ConnectionManager.GetData(query, parameters).Rows[0];
+            Customer customer = new Customer(
+                int.Parse(foundUser["customer_id"].ToString()),
+                foundUser["user_name"].ToString(),
+                foundUser["customer_name"].ToString(),
+                foundUser["password"].ToString()
+            );
+            return customer;
+        }
+
+        private bool userIsValid(string query, List<SqlParameter> parameters)
+        {
+            if (ConnectionManager.GetData(query, parameters).Rows.Count != 0)
+                return true;
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var createAccForm = new CreateAccount();
+            createAccForm.Show();
+        }
+    }
+}
