af5273b36c314301ea6b7462df0dd8617819e125
AbelG101 <abelgebeyehu101@gmail.com>
2022-10-13 21:55:58 +0300

    add boiler plate files and codes

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.Forms;
+using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Customer c = new Customer();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
